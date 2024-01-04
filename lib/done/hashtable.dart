f5e5603727d6cb23e7b2dabbc1094533baeac2bb
Dhananjay Hareesh <dhananjay.hareesh@gmail.com>
2024-01-04 16:20:12 +0530

    updated

---
+++ lib/done/hashtable.dart
@@ -0,0 +1,69 @@
+class Node {
+  String key;
+  dynamic data;
+  Node? next;
+  Node(this.key, this.data);
+}
+
+class HashTable {
+  List<Node?>? table;
+  int size;
+  HashTable(this.size) {
+    table = List.generate(size, (index) => null);
+  }
+
+  add(String key, dynamic data) {
+    int index = key.hashCode % size;
+    Node newNode = Node(key, data);
+    if (table?[index] == null) {
+      table?[index] = newNode;
+    } else {
+      newNode.next = table?[index];
+      table?[index] = newNode;
+    }
+  }
+
+  get(String key) {
+    int index = key.hashCode % size;
+    if (table?[index] == null) {
+      print('empty');
+    } else {
+      Node? currentNode = table?[index];
+      while (currentNode != null) {
+        if (currentNode.key == key) {
+          print('Value for key $key at index $index: ${currentNode.data}');
+          return;
+        }
+        currentNode = currentNode.next;
+      }
+      print('Key $key not found at index $index');
+    }
+  }
+
+  getAll(table) {
+    for (int i = 0; i < table.length; i++) {
+      print('At index $i:');
+      Node? currentNode = table?[i];
+      while (currentNode != null) {
+        print('${currentNode.data}');
+        currentNode = currentNode.next;
+      }
+      if (table[i] == null) {
+        print('No value found');
+      }
+    }
+  }
+}
+
+void main() {
+  HashTable hash = HashTable(5);
+  hash.add('dhan', 21);
+  hash.add('prrr', 32);
+  hash.add('dfsdsss', 75);
+  hash.add('rwea', 22);
+  hash.add('extra', 42);
+  hash.get('prrr');
+  hash.get('dfsdsss');
+  hash.get('rwea');
+  hash.getAll(hash.table);
+}
---
+++ lib/done/sss.dart
@@ -0,0 +1,93 @@
+class Node {
+  String key;
+  dynamic data;
+  Node? next;
+  Node(this.key, this.data);
+}
+
+class HashTable {
+  List<Node?>? table;
+  int size;
+  HashTable(this.size) {
+    table = List.generate(size, (index) => null);
+  }
+
+  add(String key, dynamic data) {
+    int index = key.hashCode % size;
+    Node newNode = Node(key, data);
+    if (table?[index] == null) {
+      table?[index] = newNode;
+    } else {
+      newNode.next = table?[index];
+      table?[index] = newNode;
+    }
+  }
+
+  remove(String key) {
+    int index = key.hashCode % size;
+    if (table?[index] != null) {
+      if (table?[index]?.next == null) {
+        table?[index] = null;
+        print('element removed');
+        return;
+      } else {
+        Node? temp = table?[index];
+        Node? prev;
+        while (temp != null) {
+          if (temp.key == key) {
+            prev?.next = temp.next;
+            print('removed');
+            return;
+          }
+          prev = temp;
+          temp = temp.next;
+        }
+      }
+    } else {
+      print('value not found');
+    }
+  }
+
+  get(String key) {
+    int index = key.hashCode % size;
+    if (table?[index] == null) {
+      print('empty');
+    } else {
+      print(table?[index]?.data);
+    }
+  }
+
+  getAll(table) {
+    for (int i = 0; i < table.length; i++) {
+      if (table[i] != null) {
+        print('At index $i = ${table?[i].data} ');
+      } else {
+        print('At index $i there is no value found');
+      }
+    }
+  }
+
+  void update(String key, dynamic newData) {
+    int index = key.hashCode % size;
+    Node? temp = table?[index];
+    while (temp != null) {
+      if (temp.key == key) {
+        temp.data = newData;
+        print('Value updated');
+        return;
+      }
+      temp = temp.next;
+    }
+    print('Value not found');
+  }
+}
+
+void main() {
+  HashTable hash = HashTable(5);
+  hash.add('dhan', 21);
+  hash.add('prrrh', 32);
+  hash.add('df', 75);
+  hash.add('rweajjjjjjj', 22);
+
+  hash.getAll(hash.table);
+}
